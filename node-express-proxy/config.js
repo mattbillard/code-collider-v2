dac08355aeb467d3506c0cfdea6c192366520b89
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-12-09 17:51:12 -0500

    Reorg - move proxy into subfolder

---
+++ node-express-proxy/config.js
@@ -0,0 +1,13 @@
+const PORT = 8000;
+
+// Inject this...
+const WEBSITE2 = 'http://localhost:3000';
+
+// ...into this
+const WEBSITE1 = 'https://www.inrhythm.com';
+
+module.exports = {
+  PORT,
+  WEBSITE1,
+  WEBSITE2,
+};
